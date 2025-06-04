1ed4479f6cab048c17c17625440fcee8c539c8c2
Aleksandra <efremova_aleksandra@yahoo.com>
2025-06-04 19:32:18 +0200

    Add Iridescence background effect

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,17 @@ import { Interests } from "./components/Interests";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
+import Iridescence from "./components/Iridescence";
 
 function App() {
   return (
     <div className="min-h-screen bg-white">
+      <Iridescence
+        color={[0, 1, 1]}
+        mouseReact={false}
+        amplitude={0.1}
+        speed={1.0}
+      />
       <Header />
       <main className="container mx-auto px-4 py-8 max-w-4xl">
         <About />
